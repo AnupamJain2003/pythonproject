32635ec568caf90fc43ac28e66f056b10a22cbfa
ANUPAM JAIN <anupamayushij@gmail.com>
2022-05-26 09:47:53 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,58 @@
+import better_profanity
+import pyautogui
+
+def interfarence():
+    text = input("Enter a string")
+    text=text.split()
+    fil1 = open("C:\\Users\\Anupam Jain\\PycharmProjects\\pythonProject2\\allowedword.text", 'w+')
+    fi = file.read()
+    fi = fi.split()
+    c = 1
+    for i in fi:
+        if (fi == world):
+            c = 1
+    if (c != 1)
+        break
+    else:
+        profanity.censor(text, '*')
+def report():
+    world=input("Enter the world you want to report")
+    f=open(" C:\Users\Anupam_Jain\PycharmProjects\pythonProject2\allowed.txt",'r')
+    d=file.read()
+
+print("1. For using interferance")
+print("2.For customizing your profinity extension")
+print("3.To add a new worl to the dictionary")
+a=int(input("Enter your choice"))
+if(a==1):
+    interfarence()
+
+if(a==2):
+    file=open("C:\Users\Anupam_Jain\PycharmProjects\pythonProject2\use.txt","r")
+    f=file.read()
+    y=input("Enter the username:")
+    k=input("Enter the password:")
+    t=[]
+    y=[]
+    d=1
+    for i in range(1,len(f)):
+        if(l[i]=='|')
+            l=1
+            g=f[d:l]
+            if
+    world=input("Enter the world you want to be linent about")
+    fil1=open("C:\\Users\\Anupam Jain\\PycharmProjects\\pythonProject2\\allowedword.text",'w+')
+    fi=file.read()
+    fi=fi.split()
+    c=1
+    for i in fi:
+        if(fi==world):
+            c=1
+    if(c!=1)
+        file.write(world)
+    else
+        print("The world already exsist in the list")
+if(c==3):
+    text1 = input("Enter the world you want to enter in the ")
+    text=[text1]
+    better_profanity.profanity.add_censor_words(text1)
